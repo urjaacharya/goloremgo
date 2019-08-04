d19740440b1ae8b407567ef928319aba85c38ff5
Urja Acharya <urja.acharya@huskers.unl.edu>
2019-08-04 08:37:50 -0700

    added functionality to create random words, sentences and paragraphs

---
+++ create-random/createrandom.go
@@ -0,0 +1,43 @@
+package createrandom
+
+import (
+	"strings"
+)
+
+var sentences = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Imperdiet sed euismod nisi porta lorem mollis aliquam. Aliquet enim tortor at auctor urna nunc id. Libero volutpat sed cras ornare arcu dui vivamus arcu felis. In dictum non consectetur a erat nam. Ut pharetra sit amet aliquam. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla."
+
+//WordArray test
+func WordArray(sentences string, n int) []string {
+	words := strings.Split(sentences, " ")
+	var finalWords []string
+	for i := 0; i < n; i++ {
+		if !(strings.HasSuffix(words[i], ",") || strings.HasSuffix(words[i], ".")) {
+			finalWords = append(finalWords, words[i])
+		}
+	}
+	return finalWords
+}
+
+func sentenceArray(n int) []string {
+	sentencesArray := strings.Split(sentences, ".")
+	var finalSentences []string
+	for i := 0; i < n; i++ {
+		finalSentences = append(finalSentences, sentencesArray[i])
+	}
+	return finalSentences
+}
+
+func para(n int) [][]string {
+	firstPara := sentenceArray(8)
+	var finalParas [][]string
+	for i := 0; i < n; i++ {
+		finalParas = append(finalParas, firstPara)
+	}
+	return finalParas
+}
+
+// func main() {
+// 	//word_array(10)
+// 	//sentence_array(2)
+// 	fmt.Println(para(6))
+//}
---
+++ create-random/templates/template_blog.md
@@ -0,0 +1,7 @@
+---
+title: {{ sent 5 }}
+tags: {{ word 3}}
+---
+
+# {{ word 4 }}
+{{ para 4 4 }}
