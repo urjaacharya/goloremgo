fc73cd4c9208d2939508c0afd5086ff051fa20df
Darshan Baral <darshanbaral@gmail.com>
2019-08-18 14:07:25 -0700

    Implemented recursive search

--- src/utils/helpers/helpers.go
+++ src/utils/helpers/helpers.go
@@ -3,8 +3,10 @@ package helpers
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
+	"regexp"
 	"time"
 )
 
@@ -27,13 +29,59 @@ func Usage() {
 	os.Exit(1)
 }
 
+// GetAllTemplateNames searches recursively inside 'path' and finds all templates
+func GetAllTemplateNames(p string) ([]string, bool, error) {
+	templateFound := false
+	templateNamePattern := `LFS_.+_\d+\.md$`
+	var allTemplates []string
+	dirErr := filepath.Walk(p, func(path string, info os.FileInfo, fileErr error) error {
+		if fileErr != nil {
+			return fileErr
+		}
+		matchesFormat, matchErr := regexp.MatchString(templateNamePattern, path)
+		if matchErr != nil {
+			return matchErr
+		}
+		if matchesFormat && !templateFound {
+			templateFound = true
+		}
+		if matchesFormat {
+			allTemplates = append(allTemplates, path)
+		}
+		return nil
+	})
+	return allTemplates, templateFound, dirErr
+}
+
+// GetTemplateNames searches only inside files and finds templates
+func GetTemplateNames(p string) ([]string, bool, error) {
+	templateFound := false
+	templateNamePattern := `LFS_.+_\d+\.md$`
+	var allTemplates []string
+	files, err := ioutil.ReadDir(p)
+	for _, file := range files {
+		matchesFormat, matchErr := regexp.MatchString(templateNamePattern, file.Name())
+		if matchErr != nil {
+			return nil, false, matchErr
+		}
+		if matchesFormat && !templateFound {
+			templateFound = true
+		}
+		if matchesFormat {
+			allTemplates = append(allTemplates, file.Name())
+		}
+	}
+	return allTemplates, templateFound, err
+}
+
 //ReadArgs Reads user provided arguments
-func ReadArgs() (int, string, bool) {
+func ReadArgs() (int, string, bool, bool) {
 	initialSeed := int(time.Now().UTC().UnixNano())
-	dirPath := flag.String("p", "", "REQUIRED: root directory that contains all the templates to be processed.")
-	randSeed := flag.Int("s", initialSeed, "OPTIONAL: seed to reproduce randomly generated contents.")
-	forceOverwrite := flag.Bool("f", false, "OPTIONAL: specify whether to overwrite files if they already exist.")
-	versionInfo := flag.Bool("v", false, "Display version information")
+	dirPath := flag.String("p", "", "REQUIRED: directory containing templates")
+	forceOverwrite := flag.Bool("f", false, "OPTIONAL: overwrite files or not")
+	recursive := flag.Bool("r", false, "OPTIONAL: search directories recursively")
+	randSeed := flag.Int("s", initialSeed, "OPTIONAL: seed")
+	versionInfo := flag.Bool("v", false, "version information")
 
 	flag.Usage = Usage
 	flag.Parse()
@@ -49,5 +97,5 @@ func ReadArgs() (int, string, bool) {
 	}
 
 	rootDir := filepath.FromSlash(*dirPath)
-	return *randSeed, rootDir, *forceOverwrite
+	return *randSeed, rootDir, *forceOverwrite, *recursive
 }
